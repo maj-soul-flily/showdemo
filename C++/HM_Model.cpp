cfcb81c7a2040e285955f90db446d707293f62da
maj-soul-flily <104069306+maj-soul-flily@users.noreply.github.com>
2022-05-20 15:59:36 +0800

    HM_Model

---
+++ C++/HM_Model.cpp
@@ -0,0 +1,16 @@
+// 文件名：循环结构
+// 创建时间：2022/5/20 9:15:00
+//案例：从键盘输入一个数字，玩家猜数字游戏
+
+#include <iostream>
+#include <ctime>;
+
+using namespace std;
+
+
+int main()
+{
+	system("pause");
+
+	return 0;
+}
